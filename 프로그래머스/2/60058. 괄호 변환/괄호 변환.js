4aa498c1a64462e6676f556c7bf297554c795ea3
YOOJAESEOK <69152917+yoo94@users.noreply.github.com>
2025-12-01 15:10:57 +0900

    [level 2] Title: 괄호 변환, Time: 1.23 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.js
@@ -1,39 +1,37 @@
 function solution(p) {
-  if (p === "") return "";
-
-  function isCorrect(s) {
-    let cnt = 0;
-    for (let ch of s) {
-      if (ch === "(") cnt++;
-      else cnt--;
-      if (cnt < 0) return false;
+    if(p.length < 1) return '';
+    function isCorrect(u){
+        let cnt = 0;
+        for(const char of u){
+            if(char === '(') cnt++;
+            if(char === ')') cnt--;
+            if(cnt< 0) return false;
+        }
+        return cnt === 0;
     }
-    return cnt === 0;
-  }
-
-  function splitUV(s) {
-    let cnt = 0;
-    let idx = 0;
-    for (; idx < s.length; idx++) {
-      if (s[idx] === "(") cnt++;
-      else cnt--;
-      if (cnt === 0) break;
+    function splitUV(str){
+        const s = str.split('')
+        let idx = 0;
+        let count = 0;
+        for(; idx < s.length; idx++){
+            if(s[idx] === ')') count++
+            if(s[idx] === '(') count--
+            if(count === 0) break;
+        }
+        const u = str.slice(0,idx + 1)
+        const v = str.slice(idx + 1)
+        
+        return [u,v];
     }
-    const u = s.slice(0, idx + 1);
-    const v = s.slice(idx + 1);
-    return [u, v];
-  }
-
-  const [u, v] = splitUV(p);
-  if (isCorrect(u)) {
-    return u + solution(v);
-  } else {
-    const inner = solution(v);
-    const flipped = u
-      .slice(1, -1)
-      .split("")
-      .map((c) => (c === "(" ? ")" : "("))
-      .join("");
-    return "(" + inner + ")" + flipped;
-  }
+    const [u,v] = splitUV(p);
+    
+    if(isCorrect(u)){
+        return u + solution(v)
+    }else{
+        const value = u.slice(1,-1).split('').map((val)=>{
+            return val ==='(' ? ')' : '('
+        }).join('')
+        return "("+solution(v)+")"+value
+    }
+    
 }
